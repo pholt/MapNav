a1bffd354b577b937e3716341dc2beb16fe7a0a7
Philip Holt <philipholt14@gmail.com>
2020-11-05 22:27:01 -0800

    Added Angular Material.

--- MapNav/MapNavFE/src/app/app.module.ts
+++ MapNav/MapNavFE/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapNavService } from '../services/map-nav.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { MapNavService } from '../services/map-nav.service';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [MapNavService],
   bootstrap: [AppComponent]
